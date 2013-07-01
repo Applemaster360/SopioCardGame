a3ced76b2b0697e5418f9f673f294099a8c2c451
Applemaster360 <Student@scs-idua-strn-09.stanford.edu>
2013-07-01 11:15:43 -0700

    Initial Commit

---
+++ SopioCardGame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SopioCardGame
+//
+//  Created by iD Student on 7/1/13.
+//  Copyright (c) 2013 James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
